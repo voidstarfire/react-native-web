3f31b40066d8e9740ad2db05f833ae602b50b6a9
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-11-17 12:37:41 -0800

    Update to flow-bin@0.78
    Ref #1172

--- packages/react-native-web/src/exports/Clipboard/index.js
+++ packages/react-native-web/src/exports/Clipboard/index.js
@@ -14,6 +14,7 @@ export default class Clipboard {
   static isAvailable() {
     if (clipboardAvailable === undefined) {
       clipboardAvailable =
+        // $FlowFixMe
         typeof document.queryCommandSupported === 'function' &&
         document.queryCommandSupported('copy');
     }
